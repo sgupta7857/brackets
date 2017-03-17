478374aa59c8bdb3a80d41cb22e30cecbcddf5bf
sgupta7857 <shiv@Shivams-MacBook-Pro.local>
2017-03-17 10:57:45 -0400

    indent fixes

--- src/command/Commands.js
+++ src/command/Commands.js
@@ -117,9 +117,8 @@ define(function (require, exports, module) {
     exports.CMD_ADD_TO_WORKINGSET_AND_OPEN  = "cmd.addToWorkingSetAndOpen";          // DocumentCommandHandlers.js   handleOpenDocumentInNewPane()
 
     // NAVIGATE
-        exports.NAVIGATE_NEXT_DOC ;// DocumentCommandHandlers.js   handleGoNextDoc()
-
-	exports.NAVIGATE_PREV_DOC ;// DocumentCommandHandlers.js   handleGoPrevDoc()
+    exports.NAVIGATE_NEXT_DOC ;// DocumentCommandHandlers.js   handleGoNextDoc()
+    exports.NAVIGATE_PREV_DOC ;// DocumentCommandHandlers.js   handleGoPrevDoc()
     exports.NAVIGATE_NEXT_DOC_LIST_ORDER    = "navigate.nextDocListOrder";           // DocumentCommandHandlers.js   handleGoNextDocListOrder()
     exports.NAVIGATE_PREV_DOC_LIST_ORDER    = "navigate.prevDocListOrder";           // DocumentCommandHandlers.js   handleGoPrevDocListOrder()
     exports.NAVIGATE_SHOW_IN_FILE_TREE  = "navigate.showInFileTree";    // DocumentCommandHandlers.js   handleShowInTree()
